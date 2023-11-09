9c72c49aa4367194a3071fe373f8c61a63229f42
Tasha D. Pais <tdp74@ilab4.cs.rutgers.edu>
2023-11-08 23:12:55 -0500

    joints are perfectly moving

--- arm_1.py
+++ arm_1.py
@@ -1,180 +1,144 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-import argparse
-import collision_checking
+import matplotlib.patches as patches
 
-# Define the arm parameters
-joint_radius = 0.05
-link_lengths = [0.4, 0.25]
-link_widths = [0.1, 0.1]
-base_position = np.array([1, 1])
 
-def forward_kinematics(theta1, theta2):
+class NLinkArm(object):
     """
-    Calculate the forward kinematics for the planar 2-joint arm.
-    Args:
-    - theta1: The angle of the first joint in radians.
-    - theta2: The angle of the second joint in radians.
-    
-    Returns:
-    - joint_positions: A list of tuples representing the positions (x, y) of each joint.
-    - end_effector_position: A tuple representing the position (x, y) of the end effector.
+    Class for controlling and plotting a planar arm with an arbitrary number of links.
     """
-    # Calculate the position of the first joint (which is fixed)
-    joint1_pos = base_position
+
+    def __init__(self, link_lengths, joint_angles, joint_radius, link_width):
+        self.n_links = len(link_lengths)
+        if self.n_links != len(joint_angles):
+            raise ValueError()
+
+        self.link_lengths = np.array(link_lengths)
+        self.joint_angles = np.array(joint_angles)
+        self.joint_radius = joint_radius
+        self.link_width = link_width
+        self.points = np.ones((self.n_links + 1, 2))
+
+        self.terminate = False
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_aspect('equal')
+        self.ax.set_xlim([0, 2])
+        self.ax.set_ylim([0, 2])
+        self.fig.canvas.mpl_connect('key_press_event', self.on_key)
+
+        self.update_points()
+        self.plot()
+
+    def update_joints(self, joint_angles):
+        self.joint_angles = joint_angles
+        self.update_points()
+
+    def transformation_matrix(self, theta, length):
+        return np.array([
+            [np.cos(theta), -np.sin(theta), length * np.cos(theta)],
+            [np.sin(theta), np.cos(theta), length * np.sin(theta)],
+            [0, 0, 1]
+        ])
     
-    # Calculate the position of the second joint
-    joint2_pos = joint1_pos + np.array([
-        link_lengths[0] * np.cos(theta1),
-        link_lengths[0] * np.sin(theta1)
-    ])
+    # transformation matrix approach
+    def update_points(self):
+        point = np.array([0, 0, 1]).reshape(3, 1)
+        prev_trans = np.identity(3) # Initialize as identity matrix
+        for i in range(self.n_links):
+            trans = self.transformation_matrix(self.joint_angles[i], self.link_lengths[i])
+            prev_trans = prev_trans @ trans
+            new_point = prev_trans @ point
+            new_point[0, 0] += self.points[0][0]
+            new_point[1, 0] += self.points[0][1]
+            self.points[i + 1][0] = new_point[0, 0]
+            self.points[i + 1][1] = new_point[1, 0]
+
+    # geometric approach
+    # def update_points(self):
+    #     for i in range(1, self.n_links + 1):
+    #         self.points[i][0] = self.points[i - 1][0] + self.link_lengths[i - 1] * np.cos(np.sum(self.joint_angles[:i]))
+    #         self.points[i][1] = self.points[i - 1][1] + self.link_lengths[i - 1] * np.sin(np.sum(self.joint_angles[:i]))
+    #     self.end_effector = np.array(self.points[self.n_links]).T
+    #     print(f"End effector: {self.end_effector}")
     
-    # Calculate the position of the end effector
-    end_effector_pos = joint2_pos + np.array([
-        link_lengths[1] * np.cos(theta1 + theta2),
-        link_lengths[1] * np.sin(theta1 + theta2)
-    ])
+
+    def rotate_joint(self, joint_idx, direction):
+        """Rotate joint by a given direction. Positive for counterclockwise, negative for clockwise."""
+        delta_angle = 5 * np.pi / 180  # 5 degrees in radians
+        self.joint_angles[joint_idx] += delta_angle * direction
+        self.update_points()
+
+    def draw_rectangle(self, start, end):
+        """Create a rectangle from start to end with a certain width."""
+        direction = np.array(end) - np.array(start)
+        length = np.linalg.norm(direction)
+        direction = direction / length
+
+        # Adjust the start and end points to account for the circle radius
+        start_adj = start + self.joint_radius * direction
+        end_adj = end - self.joint_radius * direction
+
+        # Calculate the perpendicular direction
+        perp_direction = np.array([-direction[1], direction[0]])
+        half_width_vec = 0.3 * self.link_width * perp_direction
+
+        # Calculate the 4 corners of the rectangle
+        p1 = start_adj - half_width_vec
+        p2 = start_adj + half_width_vec
+        p3 = end_adj + half_width_vec
+        p4 = end_adj - half_width_vec
+
+        return np.array([p1, p4, p3, p2, p1])
     
-    return [joint1_pos, joint2_pos], end_effector_pos
-
-def create_link_polygon(joint_start, joint_end, width):
-    """
-    Create a polygon representing a link of the arm.
+    def on_key(self, event):
+        if event.key == 'q':
+            self.terminate = True
+            plt.close()
+            return
+        elif event.key == 'z':
+            self.rotate_joint(0, -1)
+        elif event.key == 'x':
+            self.rotate_joint(0, 1)
+        elif event.key == 'c':
+            self.rotate_joint(1, -1)
+        elif event.key == 'v':
+            self.rotate_joint(1, 1)
+        self.plot()
     
-    Args:
-    - joint_start: The (x, y) starting position of the joint.
-    - joint_end: The (x, y) ending position of the joint.
-    - width: The width of the link.
-    
-    Returns:
-    - A numpy array of the vertices of the polygon representing the link.
-    """
-    # Vector from start to end
-    link_vector = np.array(joint_end) - np.array(joint_start)
-    link_length = np.linalg.norm(link_vector)
-    link_direction = link_vector / link_length
+    def run(self):
+        while not self.terminate:
+            plt.pause(0.1)
 
-    # Perpendicular vector to the link
-    perp_vector = np.array([-link_direction[1], link_direction[0]])
+    def plot(self):
+        self.ax.clear()
 
-    # Vertices of the rectangle
-    v1 = np.array(joint_start) + width / 2 * perp_vector
-    v2 = np.array(joint_start) - width / 2 * perp_vector
-    v3 = np.array(joint_end) - width / 2 * perp_vector
-    v4 = np.array(joint_end) + width / 2 * perp_vector
+        for i in range(self.n_links):
+            rectangle = self.draw_rectangle(self.points[i], self.points[i + 1])
+            self.ax.plot(rectangle[:, 0], rectangle[:, 1], 'black')
+            self.ax.fill(rectangle[:, 0], rectangle[:, 1], 'black', alpha=0.3)  # Filling the rectangle
 
-    return np.array([v1, v2, v3, v4])
+        for i in range(self.n_links + 1):
+            circle = patches.Circle(self.points[i], radius=self.joint_radius, facecolor='black')
+            self.ax.add_patch(circle)
 
-def create_joint_polygon(joint_center, radius, sides=8):
-    """
-    Create a polygon representing a joint of the arm.
-    
-    Args:
-    - joint_center: The (x, y) position of the joint center.
-    - radius: The radius of the joint circle.
-    - sides: The number of sides for the polygon approximation of the circle.
-    
-    Returns:
-    - A numpy array of the vertices of the polygon representing the joint.
-    """
-    angle = np.linspace(0, 2 * np.pi, sides, endpoint=False)
-    x = joint_center[0] + radius * np.cos(angle)
-    y = joint_center[1] + radius * np.sin(angle)
-    return np.vstack((x, y)).T
+            # # Draw local axes at each joint
+            # angle_cumulative = np.sum(self.joint_angles[:i+1])
+            # length_axis = 0.15  # Length of the axis arrows
+            # self.ax.arrow(self.points[i][0], self.points[i][1],
+            #             length_axis * np.cos(angle_cumulative), length_axis * np.sin(angle_cumulative),
+            #             head_width=0.02, head_length=0.05, fc='red', ec='red')  # X-axis
+            # self.ax.arrow(self.points[i][0], self.points[i][1],
+            #             -length_axis * np.sin(angle_cumulative), length_axis * np.cos(angle_cumulative),
+            #             head_width=0.02, head_length=0.05, fc='g', ec='g')  # Y-axis
 
-def create_arm_polygons(joint_positions, link_widths, joint_radius):
-    """
-    Create polygons for the links and joints of the robot arm.
-    
-    Args:
-    - joint_positions: A list of tuples representing the positions (x, y) of each joint.
-    - link_widths: A list of the widths of each link.
-    - joint_radius: The radius of the joints.
-    
-    Returns:
-    - A list of numpy arrays, each representing a polygon for each part of the robot arm.
-    """
-    arm_polygons = []
+        self.ax.set_xlim([0, 2])
+        self.ax.set_ylim([0, 2])
+        plt.draw()
+        plt.pause(1e-5)
 
-    # Create the polygons for the links
-    for i in range(len(joint_positions) - 1):
-        link_poly = create_link_polygon(joint_positions[i], joint_positions[i + 1], link_widths[i])
-        arm_polygons.append(link_poly)
-
-    # Create the polygons for the joints
-    for pos in joint_positions:
-        joint_poly = create_joint_polygon(pos, joint_radius)
-        arm_polygons.append(joint_poly)
-
-    return arm_polygons
-
-def plot_arm_and_obstacles(arm_polygons, obstacle_polygons):
-    """
-    Plot the robot arm and the obstacles in the workspace.
-    
-    Args:
-    - arm_polygons: A list of numpy arrays representing the robot arm's parts.
-    - obstacle_polygons: A list of numpy arrays representing the obstacles.
-    """
-    fig, ax = plt.subplots(dpi=100)
-    ax.set_aspect('equal')
-
-    # Plot the obstacles
-    for poly in obstacle_polygons:
-        polygon = Polygon(poly, closed=True, color='red', alpha=0.5)
-        ax.add_patch(polygon)
-
-    # Plot the arm
-    for poly in arm_polygons:
-        polygon = Polygon(poly, closed=True, color='blue', alpha=0.8)
-        ax.add_patch(polygon)
-
-    # Setting the limits of the plot
-    ax.set_xlim(0, 2)
-    ax.set_ylim(0, 2)
-
-    plt.show()
-
-
-def is_configuration_collision_free(arm_polygons, obstacle_polygons):
-    # Check if any of the arm's polygons collide with the obstacles
-    for arm_poly in arm_polygons:
-        for obs_poly in obstacle_polygons:
-            if collision_checking.collides(arm_poly, obs_poly):
-                return False
-    return True
-
-def generate_random_configuration():
-    theta1 = np.random.uniform(-np.pi, np.pi)
-    theta2 = np.random.uniform(-np.pi, np.pi)
-    return theta1, theta2
-
-def main(map_file):
-    # Load the map of polygons
-    obstacle_polygons = np.load(map_file, allow_pickle=True)
-
-    # Initialize a list to store collision-free configurations
-    collision_free_configs = []
-    num_configs = 5
-
-    # Generate collision-free configurations
-    while len(collision_free_configs) < num_configs:
-        theta1, theta2 = generate_random_configuration()
-        joint_positions, end_effector_position = forward_kinematics(theta1, theta2)
-        arm_polygons = create_arm_polygons(joint_positions, link_widths, joint_radius)
-        
-        if is_configuration_collision_free(arm_polygons, obstacle_polygons):
-            collision_free_configs.append((theta1, theta2))
-            # Plot the collision-free configuration
-            plot_arm_and_obstacles(arm_polygons, obstacle_polygons)
-            print(f"Found collision-free configuration: θ1={theta1}, θ2={theta2}")
-
-    print(f"Generated {num_configs} collision-free configurations.")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Generate random collision-free configurations for a robot arm.')
-    parser.add_argument('--map', type=str, help='Path to the map file.')
-    args = parser.parse_args()
-    main(args.map)
+
+    arm = NLinkArm([0.4, 0.25], [0.0, 0.0], joint_radius=0.05, link_width=0.1)
+    
+    arm.run()
